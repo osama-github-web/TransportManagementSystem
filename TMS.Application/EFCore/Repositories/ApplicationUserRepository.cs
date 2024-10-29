976eca97e3a2ab90e0a333863d668154ab6bdb22
Osama <mail.osama111@gmail.com>
2024-10-29 15:56:30 -0700

    Generic Response Added

--- TMS.Application/EFCore/Repositories/ApplicationUserRepository.cs
+++ TMS.Application/EFCore/Repositories/ApplicationUserRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using TMS.Domain.Entities;
+using TMS.Domain.GenericResponse;
 
 namespace IMS.Application.EFCore.Repositories;
 
@@ -14,88 +15,88 @@ public class ApplicationUserRepository
         _roleManager = roleManager;
     }
 
-    public async Task<ApplicationUser?> CreateUserAsync(ApplicationUser applicationUser)
+    public async Task<TMSResponse?> CreateUserAsync(ApplicationUser applicationUser)
     {
         var _user = await _userManager.FindByEmailAsync(applicationUser.Email);
         if (applicationUser == null || _user != null)
-            return null;
+            return new TMSResponse("User Not Found",applicationUser);
 
         var identityResult = await _userManager.CreateAsync(applicationUser, applicationUser.Password);
-        if(!identityResult.Succeeded) 
-            return null;
+        if(!identityResult.Succeeded)
+            return new TMSResponse("User Not Created", applicationUser);
 
         if (!await _roleManager.RoleExistsAsync(applicationUser.Role))
             await _roleManager.CreateAsync(new IdentityRole(applicationUser.Role));
 
         identityResult = await _userManager.AddToRoleAsync(applicationUser, applicationUser.Role);
         if (identityResult.Succeeded)
-            return null;
-        return applicationUser;
+            return new TMSResponse("User Role Not Added", applicationUser);
+        return new TMSResponse("User Create Successfully", applicationUser, true);
     }
     
-    public async Task<ApplicationUser?> UpdateUserAsync(ApplicationUser applicationUser)
+    public async Task<TMSResponse?> UpdateUserAsync(ApplicationUser applicationUser)
     {
         var _user = await _userManager.FindByEmailAsync(applicationUser.Email);
         if (applicationUser == null || _user != null)
-            return null;
-        
+            return new TMSResponse("User Not Found", applicationUser);
+
         _user?.Update(applicationUser);
 
         var identityResult = await _userManager.UpdateAsync(_user);
-        if(!identityResult.Succeeded) 
-            return null;
-        return applicationUser;
+        if(!identityResult.Succeeded)
+            return new TMSResponse("User Not Updated"+identityResult.Errors.ToString(), applicationUser);
+        return new TMSResponse("User Updated Successfully ", applicationUser, true);
     }
     
-    public async Task<ApplicationUser?> DeleteUserAsync(ApplicationUser applicationUser)
+    public async Task<TMSResponse?> DeleteUserAsync(ApplicationUser applicationUser)
     {
         var _user = await _userManager.FindByEmailAsync(applicationUser.Email);
         if (applicationUser == null || _user != null)
-            return null;
+            return new TMSResponse("User Not Found", applicationUser);
 
         var identityResult = await _userManager.DeleteAsync(_user);
-        if(!identityResult.Succeeded) 
-            return null;
-        return applicationUser;
+        if(!identityResult.Succeeded)
+            return new TMSResponse("User Not Deleted", applicationUser);
+        return new TMSResponse("User Deleted Successfully ", applicationUser,true);
     }
     
-    public async Task<ApplicationUser?> UpdateUserPasswordAsync(ApplicationUser applicationUser)
+    public async Task<TMSResponse?> UpdateUserPasswordAsync(ApplicationUser applicationUser)
     {
         var _user = await _userManager.FindByEmailAsync(applicationUser.Email);
         if (applicationUser == null || _user != null)
-            return null;
+            return new TMSResponse("User Not Found", applicationUser);
 
         var identityResult = await _userManager.RemovePasswordAsync(_user);
-        if(!identityResult.Succeeded) 
-            return null;
-        
+        if(!identityResult.Succeeded)
+            return new TMSResponse("User Password Not Removed "+identityResult.Errors.ToString(), applicationUser);
+
         identityResult = await _userManager.AddPasswordAsync(_user,applicationUser.Password);
-        if(!identityResult.Succeeded) 
-            return null;
-        return applicationUser;
+        if(!identityResult.Succeeded)
+            return new TMSResponse("User Password Not Updated", applicationUser);
+        return new TMSResponse("User  Password Updated Successfully ", applicationUser, true);
     }
     
-    public async Task<ApplicationUser?> AddRoleToUserAsync(ApplicationUser applicationUser)
+    public async Task<TMSResponse?> AddRoleToUserAsync(ApplicationUser applicationUser)
     {
         var _user = await _userManager.FindByEmailAsync(applicationUser.Email);
         if (applicationUser == null || _user != null)
-            return null;
-        
+            return new TMSResponse("User Not Found", applicationUser);
+
         var identityResult = await _userManager.AddToRoleAsync(_user,applicationUser.Role);
-        if(!identityResult.Succeeded) 
-            return null;
-        return applicationUser;
+        if (!identityResult.Succeeded)
+            return new TMSResponse("User Role Not Updated"+identityResult.Errors.ToString(), applicationUser);
+        return new TMSResponse("User Role Updated Successfully ", applicationUser, true);
     }
-    
-    public async Task<ApplicationUser?> RemoveRoleFromUserAsync(ApplicationUser applicationUser)
+
+    public async Task<TMSResponse?> RemoveRoleFromUserAsync(ApplicationUser applicationUser)
     {
         var _user = await _userManager.FindByEmailAsync(applicationUser.Email);
         if (applicationUser == null || _user != null)
-            return null;
-        
+            return new TMSResponse("User Not Found", applicationUser);
+
         var identityResult = await _userManager.RemoveFromRoleAsync(_user,applicationUser.Role);
-        if(!identityResult.Succeeded) 
-            return null;
-        return applicationUser;
+        if (!identityResult.Succeeded)
+            return new TMSResponse("User Role Not Removed" + identityResult.Errors.ToString(), applicationUser);
+        return new TMSResponse("User Role Removed Successfully ", applicationUser, true);
     }
 }
---
+++ TMS.Domain/GenericResponse/TMSResponse.cs
@@ -0,0 +1,27 @@
+﻿namespace TMS.Domain.GenericResponse;
+
+public class TMSResponse
+{
+    public TMSResponse(string? message = null, Object? data = null, bool isCompleted = false, TMSException? exception = null)
+    {
+        this.Message = message;
+        this.Data = data;
+        this.IsCompleted = isCompleted;
+        this.Exception = exception;
+    }
+
+    public TMSResponse() { }
+
+    public string? Message { get; set; }
+    public Object? Data { get; set; }
+    public bool IsCompleted { get; set; }
+    public TMSException? Exception { get; set; }
+}
+
+
+public class TMSException : Exception
+{
+    public TMSException(string message) : base(message: message) { }
+
+    public TMSException() { }
+}
